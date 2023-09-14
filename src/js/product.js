8477794430de39fed170d5872278f668c8f50318
Bob Hathaway <113385106+BobHathaway2@users.noreply.github.com>
2023-09-13 19:51:02 -0600

    changed add cart functionality to add an array

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
+let productArray = [];
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartContent = getLocalStorage("so-cart");
+  if (cartContent) {
+    productArray = cartContent;
+  }
+    productArray.push(product);
+    setLocalStorage("so-cart", productArray);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
