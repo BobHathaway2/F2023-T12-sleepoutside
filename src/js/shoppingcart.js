c25171d80057dfaf7ad605cdc9e59f235c239d5b
reaganhsmith <reagansmith@byui.edu>
2023-09-19 12:13:14 -0600

    changes for total price

---
+++ src/js/shoppingcart.js
@@ -0,0 +1,25 @@
+const total = document.querySelector(".cart-total");
+const cartFooter = document.querySelector(".cart-footer");
+const tentPrice = document.querySelectorAll("cart-card__price")
+
+
+function checkCart(){
+    const cart = localStorage.getItem("so-cart");
+    const products = JSON.parse(cart);
+    console.log(products);
+
+    if(cart !== null){
+
+        const totalPrice = products.reduce((accumulator, products) => {
+            return accumulator + products.FinalPrice;
+          }, 0);
+
+        total.textContent = `Total: $ ${totalPrice}`;
+
+    }else{
+        cartFooter.style.display = 'none';
+    }
+}
+
+
+window.addEventListener('load', checkCart);
