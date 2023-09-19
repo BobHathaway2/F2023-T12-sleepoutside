115822074f713576be9046bd05c406b8800fdb59
reaganhsmith <reagansmith@byui.edu>
2023-09-19 09:26:24 -0600

    week 2 work

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -18,20 +18,6 @@ function productDetailsTemplate(product) {
     </div></section>`;
 }
 
-let productArray = [];
-function addProductToCart(product) {
-  let cartContent = getLocalStorage("so-cart");
-  if (cartContent) {
-    productArray = cartContent;
-  }
-  productArray.push(product);
-  setLocalStorage("so-cart", productArray);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -40,17 +26,18 @@ export default class ProductDetails {
     this.dataSource = dataSource;
   }
   async init() {
-    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    this.product = await this.dataSource.findProductById(this.productId);
-    // once we have the product details we can render out the HTML
-    this.renderProductDetails("main");
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    document
-      .getElementById("addToCart")
-      .addEventListener("click", addToCartHandler);
-  }
 
+    this.product = await this.dataSource.findProductById(this.productId);
+
+    this.renderProductDetails("main");
+
+    document
+    .getElementById("addToCart")
+    .addEventListener("click", this.addToCart.bind(this));
+}
+addToCart() {
+  setLocalStorage("so-cart", this.product);
+}
 
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
