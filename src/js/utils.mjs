613fa9e8cd355543cfed149f5a4cec0c51576e0e
reaganhsmith <reagansmith@byui.edu>
2023-09-20 09:58:31 -0600

    week 2 team work

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,20 +22,6 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-export function renderListWithTemplate(
-  templateFn,
-  parentElement,
-  list,
-  position = "afterbegin",
-  clear = false
-) {
-  const htmlStrings = list.map(templateFn);
-  // if clear is true we need to clear out the contents of the parent.
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-}
 
 
 export function getParams(param){
