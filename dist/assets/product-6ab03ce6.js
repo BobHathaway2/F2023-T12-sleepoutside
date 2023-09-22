ed1a5737fb969858acde61ed15aef0e422059882
Erubami <erubamigodstime43@gmail.com>
2023-09-22 16:27:24 -0700

    my test branch created

--- dist/assets/product-6ab03ce6.js
+++ dist/assets/product-6ab03ce6.js
@@ -1,4 +1,23 @@
-import{g as s,s as d,a as e}from"./utils-14f1728a.js";function c(t){if(t.ok)return t.json();throw new Error("Bad Response")}class i{constructor(r){this.category=r,this.path=`../json/${this.category}.json`}getData(){return fetch(this.path).then(c).then(r=>r)}async findProductById(r){return(await this.getData()).find(o=>o.Id===r)}}function n(t){return`<section class="product-detail"> <h3>${t.Brand.Name}</h3>
+import { g as s, s as d, a as e } from "./utils-14f1728a.js";
+function c(t) {
+  if (t.ok) return t.json();
+  throw new Error("Bad Response");
+}
+class i {
+  constructor(r) {
+    (this.category = r), (this.path = `../json/${this.category}.json`);
+  }
+  getData() {
+    return fetch(this.path)
+      .then(c)
+      .then((r) => r);
+  }
+  async findProductById(r) {
+    return (await this.getData()).find((o) => o.Id === r);
+  }
+}
+function n(t) {
+  return `<section class="product-detail"> <h3>${t.Brand.Name}</h3>
     <h2 class="divider">${t.NameWithoutBrand}</h2>
     <img
       class="divider"
@@ -12,4 +31,34 @@ import{g as s,s as d,a as e}from"./utils-14f1728a.js";function c(t){if(t.ok)retu
     </p>
     <div class="product-detail__add">
       <button id="addToCart" data-id="${t.Id}">Add to Cart</button>
-    </div></section>`}class u{constructor(r,a){this.productId=r,this.product={},this.dataSource=a}async init(){this.product=await this.dataSource.findProductById(this.productId),this.renderProductDetails("main"),document.getElementById("addToCart").addEventListener("click",this.addToCart.bind(this))}addToCart(){this.addProduct(this.product)}addProduct(r){this.productArray||(this.productArray=[]);let a=s("so-cart");a&&(this.productArray=a),this.productArray.push(r),d("so-cart",this.productArray)}renderProductDetails(r){document.querySelector(r).insertAdjacentHTML("afterBegin",n(this.product))}}const h=new i("tents"),l=e("product"),p=new u(l,h);p.init();
+    </div></section>`;
+}
+class u {
+  constructor(r, a) {
+    (this.productId = r), (this.product = {}), (this.dataSource = a);
+  }
+  async init() {
+    (this.product = await this.dataSource.findProductById(this.productId)),
+      this.renderProductDetails("main"),
+      document
+        .getElementById("addToCart")
+        .addEventListener("click", this.addToCart.bind(this));
+  }
+  addToCart() {
+    this.addProduct(this.product);
+  }
+  addProduct(r) {
+    this.productArray || (this.productArray = []);
+    let a = s("so-cart");
+    a && (this.productArray = a),
+      this.productArray.push(r),
+      d("so-cart", this.productArray);
+  }
+  renderProductDetails(r) {
+    document.querySelector(r).insertAdjacentHTML("afterBegin", n(this.product));
+  }
+}
+const h = new i("tents"),
+  l = e("product"),
+  p = new u(l, h);
+p.init();
--- src/js/shoppingcart.js
+++ src/js/shoppingcart.js
@@ -1,20 +1,19 @@
 const total = document.querySelector(".cart-total");
 const cartFooter = document.querySelector(".cart-footer");
-const tentPrice = document.querySelectorAll("cart-card__price")
+const tentPrice = document.querySelectorAll("cart-card__price");
 
+function checkCart() {
+  const cart = localStorage.getItem("so-cart");
+  const products = JSON.parse(cart);
 
-function checkCart(){
-    const cart = localStorage.getItem("so-cart");
-    const products = JSON.parse(cart);
-
-    if(cart !== null){
-        const totalPrice = products.reduce((accumulator, products) => {
-            return accumulator + products.FinalPrice;
-          }, 0);
-        total.textContent = `Total: $ ${totalPrice}`;
-    }else{
-        cartFooter.style.display = 'none';
-    }
+  if (cart !== null) {
+    const totalPrice = products.reduce((accumulator, products) => {
+      return accumulator + products.FinalPrice;
+    }, 0);
+    total.textContent = `Total: $ ${totalPrice}`;
+  } else {
+    cartFooter.style.display = "none";
+  }
 }
 
-window.addEventListener('load', checkCart);
+window.addEventListener("load", checkCart);
