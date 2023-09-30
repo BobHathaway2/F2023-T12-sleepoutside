380ef7ad2ddce62e11252fd097044fa5bac06ba8
Bob Hathaway <113385106+BobHathaway2@users.noreply.github.com>
2023-09-30 09:40:19 -0600

    if cart empty, don't map the array.

--- src/js/superScript.mjs
+++ src/js/superScript.mjs
@@ -7,6 +7,9 @@ import { getLocalStorage } from "./utils.mjs";
 export default function totalAmount(){      
     const array = getLocalStorage("so-cart")
     const cartAmount = document.getElementById("cartAmount");
-    cartAmount.innerHTML = array.map((x) => x.quantity).reduce((x, y) => x + y, 0)   
-
+    if (array) {
+        cartAmount.innerHTML = array.map((x) => x.quantity).reduce((x, y) => x + y, 0); 
+    } else {
+        cartAmount.innerHTML = 0;
+    }
 }
