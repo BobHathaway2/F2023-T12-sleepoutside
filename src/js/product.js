b8407fd71ca954f7b40dc506902e7f9ecd6bf07d
reaganhsmith <reagansmith@byui.edu>
2023-09-30 19:26:09 -0600

    changes for individual activity 3

--- src/js/product.js
+++ src/js/product.js
@@ -10,24 +10,3 @@ const productId = getParams("product");
 const product = new productDetails(productId, dataSource);
 product.init();
 
-// let productArray = [];
-// // const dataSource = new ProductData("tents");
-
-// function addProductToCart(product) {
-//   let cartContent = getLocalStorage("so-cart");
-//   if (cartContent) {
-//     productArray = cartContent;
-//   }
-//   productArray.push(product);
-//   setLocalStorage("so-cart", productArray);
-// }
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById('addToCart')
-//   .addEventListener('click', addToCartHandler);
--- src/js/shoppingcart.mjs
+++ src/js/shoppingcart.mjs
@@ -13,11 +13,11 @@ function checkCart(){
           //return accumulator + products.FinalPrice;
           return accumulator + (products.ListPrice - (products.Discount ?? 0));
         }, 0);
-      if(totalPrice > 0){
-      total.textContent = `Total: $ ${totalPrice}`;}
-      else{
-        totalPrice.style.display ="none"
-      }
+      if(totalPrice >0){total.textContent = `Total: $ ${totalPrice}`;
+    }else{
+      total.style.display = "none";
+    }
+      
   }else{
       cartFooter.style.display = "none";
   }
@@ -28,7 +28,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimarySmall}"
       alt="${item.Name}"
     />
   </a>
@@ -37,7 +37,7 @@ function cartItemTemplate(item) {
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <span class="remove-item" data-id="${item.Id}">x</span>
-  <p class="cart-card__quantity">qty:${item.quantity} </p>
+  <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__list">Price: $${item.ListPrice}</p>
   <p class="cart-card__discount">Discount: $${(item.Discount ?? 0)}</p>
   <p class="cart-card__price">Final: $${item.ListPrice - (item.Discount ?? 0)}</p>  
