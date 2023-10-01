d6d53c9b9ae41e889f6cc6cba9a62b0a31f0ab5f
Daen Antule <ant21006@byui.edu>
2023-10-01 08:13:51 +0300

    try to chang qty of items in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./shoppingcart.mjs";
 import totalAmount from "./superScript.mjs";
 
@@ -7,6 +7,7 @@ loadHeaderFooter();
 let cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
 attachRemoveListeners();
+attachAddListeners();
 
 function removeItemFromCart(productId) {
   // Fetch from local storage
@@ -43,4 +44,29 @@ function attachRemoveListeners() {
       removeItemFromCart(productId);
     });
   });
+}
+
+// Function to add an item to the cart based on quantity
+function addItemToCart(productId) {
+  const cartItems = getLocalStorage("so-cart");
+  const itemIndex = cartItems.findIndex(item => item.Id === productId);
+  
+  if (itemIndex !== -1) {
+      cartItems[itemIndex].quantity += 1;
+      setLocalStorage("so-cart", cartItems);
+      cart = new ShoppingCart("so-cart", ".product-list");
+      cart.renderCartContents();
+      attachAddListeners();
+
+  }
+}
+// Listener for + to remove item from cart
+function attachAddListeners() {
+    const addButtons = document.querySelectorAll(".add-item");
+    addButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            const productId = e.target.getAttribute("data-id");
+            addItemToCart(productId);
+        });
+    });
 }
