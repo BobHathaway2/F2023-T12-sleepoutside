65995c18ba2f339abb82bc31ffa3c88e03c84756
reaganhsmith <reagansmith@byui.edu>
2023-09-30 20:34:02 -0600

    add filters

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,19 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
-import { loadHeaderFooter, getParams } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter, getParams } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParams('category' );
-// first create an instance of our ProductData class.
+const category = getParams("category");
+
 const dataSource = new ProductData();
-// then get the element we want the product list to render in
-const listElement = document.querySelector('.product-list');
-// then create an instance of our ProductList class and send it the correct information.
-const myList = new ProductList(category, dataSource, listElement);
-// finally call the init method to show our products
-myList.init();
+const listElement = document.querySelector(".product-list");
+const header = document.querySelector(".categoryName");
+const h2element = document.createElement("h2");
+h2element.textContent = category.toUpperCase();
+header.appendChild(h2element);
+
+const productListing = new ProductListing(category, dataSource, listElement);
+
+
+productListing.init();
