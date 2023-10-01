6b68ae36e5aa9b10996d1e18e332a3339a698424
reaganhsmith <reagansmith@byui.edu>
2023-09-30 20:12:41 -0600

    Addding sorting filters to the product-list page

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,36 +1,49 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 export default class ProductListing {
-  constructor(category, dataSource, listElement) {
-    this.category = category;
-    this.dataSource = dataSource;
-    this.listElement = listElement;
-  }
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+        this.products = []; // Store the products
+    }
 
-  async init() {
-    const products = await this.dataSource.getData(this.category);
-    // const filteredProducts = this.filterTentsById(products);
-    this.renderList(products);
-  }
+    async init() {
+        this.products = await this.dataSource.getData(this.category);
 
-  renderList(list, position = "afterbegin", clear = false) {
-    renderListWithTemplate(productCardTemplate, this.listElement, list, position, clear);
+        const abcButton = document.querySelector(".abcSort");
+        const priceSort = document.querySelector(".priceSort");
+
+        abcButton.addEventListener("click", () => this.sortABC());
+        priceSort.addEventListener("click", () => this.sortPrice());
+
+        this.renderList(this.products);
+    }
+
+    renderList(list, position = "afterbegin", clear = false) {
+        renderListWithTemplate(productCardTemplate, this.listElement, list, position, clear);
+    }
+
+    sortABC() {
+        const abcProducts = [...this.products].sort((a, b) => a.Name.localeCompare(b.Name));
+        this.renderList(abcProducts);
+    }
+
+    sortPrice() {
+        const moneySort = [...this.products].sort((a, b) => a.FinalPrice - b.FinalPrice);
+        this.renderList(moneySort);
+    }
 }
-
-  filterTentsById(tents) {
-    const tent_ids_to_keep = ["880RR", "985RF", "985PR", "344YJ"];
-    return tents.filter(tent => tent_ids_to_keep.includes(tent.Id));
-  }
-}
-
 
 function productCardTemplate(product) {
-  return `<li class="product-card">
-    <a href="../product_pages/index.html?product=${product.Id}">
-      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
-      <h3 class="card__brand">${product.Brand.Name}</h3>
-      <h2 class="card__name">${product.NameWithoutBrand}</h2>
-      <p class="product-card__price">$${product.FinalPrice}</p>
-    </a>
-  </li>`;
+    return `<li class="product-card">
+        <a href="../product_pages/index.html?product=${product.Id}">
+            <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.NameWithoutBrand}</h2>
+            <p class="product-card__price">$${product.FinalPrice}</p>
+        </a>
+    </li>`;
 }
+
+
