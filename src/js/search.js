4617621311300f88a0d16c477f130cf1deb15a35
Bob Hathaway <hat22014@byui.edu>
2023-10-06 17:44:33 -0600

    search visually there

---
+++ src/js/search.js
@@ -0,0 +1,18 @@
+import ExternalServices from "./ExternalServices.mjs";
+import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter, getParams } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const category = getParams(tents);
+
+const dataSource = new ExternalServices();
+const listElement = document.querySelector(".product-list");
+const header = document.querySelector(".categoryName");
+const h2element = document.createElement("h2");
+h2element.textContent = category.toUpperCase();
+header.appendChild(h2element);
+
+const productListing = new ProductListing(category, dataSource, listElement);
+
+productListing.init();
