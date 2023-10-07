63a9a9201b3247a8124d09a526225ccafedca2c4
Daen Antule <ant21006@byui.edu>
2023-10-08 02:09:41 +0300

    quantity fix

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, loadHeaderFooter, totalAmount, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter, totalAmount } from "./utils.mjs";
 import ShoppingCart from "./shoppingcart.mjs";
 
 loadHeaderFooter();
@@ -7,44 +7,49 @@ let cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
 attachEventListeners();
 
-// Summary header of the code goes here
-function removeItemFromCart(productId) {
-  const cartItems = getLocalStorage("so-cart");
-  const itemIndex = cartItems.findIndex((item) => item.Id === productId);
-  if (itemIndex !== -1 && cartItems[itemIndex].quantity > 1) {
-    cartItems[itemIndex].quantity -= 1;
-  } else {
-    cartItems.splice(itemIndex, 1);
-  }
-  localStorage.setItem("so-cart", JSON.stringify(cartItems));
-  totalAmount();
-  cart = new ShoppingCart("so-cart", ".product-list");
-  cart.renderCartContents();
-}
 
-// Summary header of the code goes here
-function addItemToCart(productId) {
+function adjustCartItem(productId, change) {
   const cartItems = getLocalStorage("so-cart");
   const itemIndex = cartItems.findIndex(item => item.Id === productId);
   if (itemIndex !== -1) {
-      cartItems[itemIndex].quantity += 1;
-      setLocalStorage("so-cart", cartItems);
+      if (change === -1 && cartItems[itemIndex].quantity === 1) {
+          cartItems.splice(itemIndex, 1);
+      } else {
+          cartItems[itemIndex].quantity += change;
+      }
+      localStorage.setItem("so-cart", JSON.stringify(cartItems));
   }
+  totalAmount();
   cart = new ShoppingCart("so-cart", ".product-list");
   cart.renderCartContents();
-  totalAmount();
 }
 
-// Summary header of the code goes here
+function removeProductFromCart(productId) {
+  const cartItems = getLocalStorage("so-cart");
+  const itemIndex = cartItems.findIndex(item => item.Id === productId);
+  if (itemIndex !== -1) {
+      cartItems.splice(itemIndex, 1);
+      localStorage.setItem("so-cart", JSON.stringify(cartItems));
+  }
+  totalAmount();
+  cart = new ShoppingCart("so-cart", ".product-list");
+  cart.renderCartContents();
+}
+window.removeProductFromCart = removeProductFromCart;
+
 function attachEventListeners() {
   const cartContainer = document.querySelector(".product-list");
   cartContainer.addEventListener("click", (e) => {
-    if (e.target.classList.contains("remove-item")) {
       const productId = e.target.getAttribute("data-id");
-      removeItemFromCart(productId);
-    } else if (e.target.classList.contains("add-item")) {
-      const productId = e.target.getAttribute("data-id");
-      addItemToCart(productId);
-    }
+      if (e.target.classList.contains("remove-item")) {
+          adjustCartItem(productId, -1);
+      } else if (e.target.classList.contains("add-item")) {
+          adjustCartItem(productId, +1);
+      } else if (e.target.classList.contains("remove-all")) {
+        const productIdForRemove = e.target.closest(".cart-card").querySelector(".remove-item").getAttribute("data-id");
+        if (typeof window.removeProductFromCart === "function") {
+            window.removeProductFromCart(productIdForRemove);
+        }
+      }
   });
 }
--- src/js/shoppingcart.mjs
+++ src/js/shoppingcart.mjs
@@ -3,61 +3,63 @@ import { getLocalStorage } from "./utils.mjs";
 const total = document.querySelector(".cart-total");
 const cartFooter = document.querySelector(".cart-footer");
 
-function checkCart(){
-  let cart = [];
-  let currentCartContent = localStorage.getItem("so-cart");
-    cart = currentCartContent;
-    const products = JSON.parse(cart);
-    const totalPrice = products.reduce((accumulator, products) => {
-        //return accumulator + products.FinalPrice;
-        return accumulator + (products.ListPrice * products.quantity - (products.Discount ?? 0));
-      }, 0);
+function checkCart() {
+  const cart = getLocalStorage("so-cart");
+  const totalPrice = cart.reduce((accumulator, product) => {
+      return accumulator + (product.ListPrice * product.quantity - (product.Discount ?? 0));
+  }, 0);
 
-    if(totalPrice > 0){
-    total.textContent = `Total: $ ${totalPrice}`;}
-    else{
+  if (totalPrice > 0) {
+      total.textContent = `Total: $ ${totalPrice}`;
+  } else {
       total.style.display = "none";
-    }
+  }
 }
 
-
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img src="${item.Images.PrimaryMedium}" 
-        srcset="${item.Images.PrimaryLarge} 800w, 
-                ${item.Images.PrimaryMedium} 500w, 
-                ${item.Images.PrimarySmall} 300w"
-        sizes="(min-width: 800px) 800px,
-               (min-width: 500px) 500px,
-               300px" 
-        alt="${item.Name}" />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <span class="add-item" data-id="${item.Id}">+</span>
-  <span class="remove-item" data-id="${item.Id}">x</span>
-  <p class="cart-card__quantity">qty:${item.quantity} </p>
-  <p class="cart-card__list">Price: $${item.ListPrice}</p>
-  <p class="cart-card__discount">Discount: $${(item.Discount ?? 0)}</p>
-  <p class="cart-card__price">Final: $${item.ListPrice - (item.Discount ?? 0)}</p>  
+    const colorName = item.Colors && item.Colors[0] ? item.Colors[0].ColorName : "N/A";
+    
+    return `
+<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+        <img src="${item.Images.PrimaryMedium}" 
+            srcset="${item.Images.PrimaryLarge} 800w, 
+                    ${item.Images.PrimaryMedium} 500w, 
+                    ${item.Images.PrimarySmall} 300w"
+            sizes="(min-width: 800px) 800px,
+                   (min-width: 500px) 500px,
+                   300px" 
+            alt="${item.Name}" />
+    </a>
+    <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${colorName}</p>
+    <p class="cart-card__list">Price: $${item.ListPrice}</p>
+    <p class="cart-card__discount">Discount: $${(item.Discount ?? 0)}</p>
+    <p class="cart-card__price">Final: $${item.ListPrice - (item.Discount ?? 0)}</p>
+    <div class="adjust">
+        <div class="qty-remove">
+            <span class="remove-item" data-id="${item.Id}">-</span>
+            <p class="cart-card__quantity">${item.quantity}</p>
+            <span class="add-item" data-id="${item.Id}">+</span>
+        </div>
+        <button class="remove-all">Remove</button>
+    </div>
 </li>`;
-
-  return newItem;
 }
 
 export default class ShoppingCart {
-  constructor(key, parentSelector) {
-    this.key = key;
-    this.parentSelector = parentSelector;
-  }
-  renderCartContents() {
-    const cartItems = getLocalStorage(this.key);
-    if(cartItems){const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
-    checkCart();}
-    
-  }
+    constructor(key, parentSelector) {
+        this.key = key;
+        this.parentSelector = parentSelector;
+    }
+    renderCartContents() {
+        const cartItems = getLocalStorage(this.key);
+        if (cartItems) {
+            const htmlItems = cartItems.map(cartItemTemplate);
+            document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+            checkCart();
+        }
+    }
 }
